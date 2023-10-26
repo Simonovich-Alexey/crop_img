73fec5eb75df70e15ec0c98b8841772b15b09e2f
Alexey_Sim <simonovich_alexey@bk.ru>
2023-10-26 17:55:17 +0500

    Initial commit

--- main.py
+++ main.py
@@ -3,16 +3,31 @@ from tqdm import tqdm
 from PIL import Image, ImageOps
 
 
+def check_folder(path):
+    if os.path.exists(path) is not True:
+        os.mkdir(path)
+        print('Дириктория создана!')
+
+    return path
+
+
 def crop(*size):
-    path_input = os.path.join(os.getcwd() + r'\input_img')
-    path_output = os.path.join(os.getcwd() + r'\output_img')
-    file_list = tqdm(os.listdir(path_input))
-    for i in file_list:
-        try:
-            with Image.open(f'{path_input}\\{i}') as img:
-                ImageOps.fit(img, size).save(f'{path_output}\\{i}')
-        except OSError:
-            print('Что-то пошло не так!')
+    path_input = check_folder(os.path.join(os.getcwd() + r'\input_img'))
+    path_output = check_folder(os.path.join(os.getcwd() + r'\output_img'))
+
+    if len(os.listdir(path_input)) > 1:
+        file_list = tqdm(os.listdir(path_input))
+        for i in file_list:
+            try:
+                with Image.open(f'{path_input}\\{i}') as img:
+                    ImageOps.fit(img, size).save(f'{path_output}\\{i}')
+            except OSError:
+                print('Что-то пошло не так!')
+
+            os.remove(f'{path_input}\\{i}')
+
+    else:
+        print('Дириктория пустая')
 
 
 if __name__ == '__main__':
